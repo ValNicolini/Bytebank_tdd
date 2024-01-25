b90e0e6b728d5c1c70288813daa3b071f7365fd1
ValNicolini <val_nicolini13@hotmail.com>
2024-01-25 12:22:00 -0300

    Quinta

--- codigo/main.py
+++ codigo/main.py
@@ -8,4 +8,10 @@ def teste_idade():
     funcionario_teste = Funcionario('Teste', '13/03/2002', 111)
     print(f'Teste = {funcionario_teste.idade()}')
 
+    funcionario_teste1 = Funcionario('Teste', '13/03/2001', 111)
+    print(f'Teste = {funcionario_teste1.idade()}')
+
+    funcionario_teste2 = Funcionario('Teste', '01/12/2001', 111)
+    print(f'Teste = {funcionario_teste2.idade()}')
+
 teste_idade()
