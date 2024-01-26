0d5da2468635c32cefa83bcd9175112fef72064b
ValNicolini <val_nicolini13@hotmail.com>
2024-01-26 14:58:30 -0300

    Sexta

--- testes/test_bytebank.py
+++ testes/test_bytebank.py
@@ -17,6 +17,21 @@ class TestClass:
 
         assert resultado == esperado # Then(desfecho)
 
+    def test_quando_decrescimo_salario_recebe_100000_deve_retornar_90000(self):
+        entrada = 100000  # Given(contexto)
+        entrada_nome = 'Giovana Silva'
+        esperado = 90000
+
+        funcionario_teste = Funcionario(entrada_nome, '03/03/1974',  entrada)
+        funcionario_teste.decrescimo_salario() # When(ação)
+        resultado = funcionario_teste.salario
+
+        assert resultado == esperado  # Then(desfecho)
+
+
+
+
+
 
 
 
